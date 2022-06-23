9f5f18822e4b45944127b8221cc36c7ef6c2717b
1230liutinghui <65661851+1230liutinghui@users.noreply.github.com>
2022-06-23 09:07:00 +0800

    Update record.sql

--- record.sql
+++ record.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80023
  File Encoding         : 65001
 
- Date: 16/06/2022 16:12:24
+ Date: 21/06/2022 00:22:11
 */
 
 SET NAMES utf8mb4;
@@ -28,13 +28,8 @@ CREATE TABLE `record`  (
   `score` int NULL DEFAULT NULL,
   `start_time` datetime(6) NULL DEFAULT NULL,
   `end_time` datetime(6) NULL DEFAULT NULL,
+  `content` varchar(10000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of record
--- ----------------------------
-INSERT INTO `record` VALUES (1, 12312, '1912033', 53, '2022-06-15 14:20:28.000000', '2022-06-30 14:20:33.000000');
-INSERT INTO `record` VALUES (3, 3212321, '2343', 67, '2022-06-22 14:53:27.000000', '2022-06-25 14:53:30.000000');
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
